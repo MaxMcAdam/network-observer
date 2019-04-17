0b70ddbe6bf8c7f86d2ee95cca1ef68d45900fbf
Maxwell-McAdam <Maxwell.McAdam@ibm.com>
2019-04-16 21:48:21 -0400

    removed extra .txt files

--- discovery.go
+++ discovery.go
@@ -2,25 +2,56 @@ package main
 
 import (
 	"bufio"
-	_ "bytes"
+	"bytes"
 	_ "encoding/json"
 	"fmt"
 	_ "io"
 	_ "net"
 	"os/exec"
+  "os"
+	"sync"
+  "net"
+  "encoding/xml"
+  "io/ioutil"
+  "net/http"
+  "encoding/json"
 )
 
 func main() {
-	//networkChanges := make(chan int)
-	go discovery()
-
-	//msg := <-networkChanges
-	//fmt.Println(msg)
+  url := "http://127.0.0.1:5984/"
+  addrs:=getNetwork()
+  var wg sync.WaitGroup
+  for _,address := range addrs {
+    wg.Add(1)
+    fmt.Println(address)
+    go discovery(&wg, address)
+    findChanges(parseNmap(), url)
+  }
+	wg.Wait()
 }
 
-func discovery() {
-	networkMonitor := exec.Command("./test.sh")
-	networkChanges, err := networkMonitor.StdoutPipe()
+func getNetwork() []string{
+  network := make([]string, 0)
+  netInterfaces, _ := net.Interfaces()
+  for _, interf := range netInterfaces{
+    if addrs, err := interf.Addrs(); err == nil{
+      for index,address := range addrs {
+        if(interf.Name == "en0" && index == 1){
+          network = append(network, address.String())
+        }
+      }
+    }
+  }
+  return network
+}
+
+func discovery(wg *sync.WaitGroup, ipAddr string) {
+	fmt.Println("start goroutine")
+  cmdFunction := "./service.sh"
+  cmdArgs := ipAddr
+	cmd := exec.Command(cmdFunction, cmdArgs)
+	networkChanges, err := cmd.StdoutPipe()
+	cmd.Start()
 	if err != nil {
 		fmt.Println("Error creating the standard output pipe")
 		panic(err)
@@ -30,8 +61,72 @@ func discovery() {
 		fmt.Println(scanner.Text())
 	}
 	fmt.Println("end goroutine")
+	wg.Done()
 }
 
-func db_changes(devicesChanged []string) {
+func parseNmap() []Host{
+  xmlFile, err := os.Open("scan.xml")
 
+  if err != nil {
+    fmt.Println(err)
+  }
+  byteValue, _ := ioutil.ReadAll(xmlFile)
+
+  var scan NmapRun
+
+  xml.Unmarshal(byteValue, &scan)
+
+  defer xmlFile.Close()
+  return scan.Hosts
+}
+
+func findChanges(liveHosts []Host, dbURL string) {
+  for _,host := range liveHosts {
+    queryLiveHosts(host, dbURL)
+    if(len(host.Hostnames) > 0){
+      fmt.Println(host.Hostnames[0].Name)
+    } else {
+      fmt.Println("no name")
+    }
+  }
+}
+
+func queryLiveHosts(host Host, url string) bool{
+  jsonStr := map[string]string{"addr":host.Addresses[0].Addr}
+  jsonValue, _ := json.Marshal(jsonStr)
+  resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+  if err!=nil{
+    return false
+  }
+  fmt.Println(resp)
+  return true
+}
+
+
+type NmapRun struct {
+  XMLName xml.Name `xml:"nmaprun" json:"nmaprun"`
+  Hosts []Host `xml:"host" json:"host"`
+}
+
+type Host struct {
+  XMLName xml.Name `xml:"host" json:"host"`
+  Status []Status `xml:"status" json:"status"`
+  Addresses []Address `xml:"address" json:"address"`
+  Hostnames []Hostname `xml:"hostnames>hostname" json:"hostnames"`
+}
+
+type Hostname struct {
+  Name string `xml:"name,attr" json:"name"`
+  Type string `xml:"type,attr" json:"type"`
+}
+
+type Status struct {
+  State string `xml:"state,attr" json:"state"`
+  Reason string `xml:"reason,attr" json:"reason"`
+  ReasonTTL float32 `xml:"reason_ttl,attr" json:"reason_ttl"`
+}
+
+type Address struct {
+  Addr string `xml:"addr,attr" json:"addr"`
+  AddrType string `xml:"addrtype,attr" json:"addrtype"`
 }
--- service.sh
+++ service.sh
@@ -1,48 +1,5 @@
 #!/usr/bin/env bash
 
-host_ip=$(ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d ' ' -f 2)
-subnet=$(ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d ' ' -f 4 | cut -d 'x' -f 2)
-subnet_cidr=0
-#echo "host ip $host_ip"
-#echo "subnet $subnet"
-if [ "$(echo $subnet | cut -c 1)" = "f" ]; then
-  while read -n 1 i; do
-    case $i in
-      [f]*)
-      let "subnet_cidr+=4"
-      ;;
-      [e]*)
-      let "subnet_cidr+=3"
-      ;;
-      [c]*)
-      let "subnet_cidr+=2"
-      ;;
-      [8]*)
-      let "subnet_cidr+=1"
-      ;;
-    esac
-  done <<< "$subnet"
-else
-  subnet_cidr=24
-fi
+ipAddr="$1"
 
-#while true; do
-  mv ./new-scan.txt ./last-scan.txt
-
-  nmap -sn -oX - $host_ip/$subnet_cidr | grep 'Nmap scan' > new-scan.txt
-
-  diff new-scan.txt last-scan.txt > delta.txt
-
-  while read line; do
-    device_name = $(echo line | cut -d ' ' -f 5)
-    device_ip = $(echo line | cut -d ' ' -f 6)
-    current_time = date
-    if [$(echo $line | cut -d ' ' -f 1) = '<']; then
-      #curl -X POST http://127.0.0.1:5984/devices_online -H "Content-Type: application/json" -d '{"device-name":"$device_name", "device-ip":"$device_ip", "time-discovered":"$current_time"}'
-      echo '{"device-name":"$device_name", "device-ip":"$device_ip", "time-discovered":"$current_time"}'
-    fi
-  done < delta.txt
-  grep '<' delta.txt > devices-added.txt
-  grep '>' delta.txt > devices-removed.txt
-  cat delta.txt | echo
-#done
+nmap -sn -oX - $ipAddr > scan.xml
