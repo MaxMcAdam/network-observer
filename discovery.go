09c19834a6f2665028335e44176d486979ef5052
Maxwell-McAdam <Maxwell.McAdam@ibm.com>
2019-04-17 21:15:45 -0400

    fixed selector address to ipaddress json tag

--- discovery.go
+++ discovery.go
@@ -151,11 +151,11 @@ func queryLiveHosts(host Host, url string) (bool, string, string){
   for _,address := range host.Addresses{
     type AddrSelector struct{
       Selector struct {
-        Addr string `json:"address.addr"`
+        Addr string `json:"ipaddress.addr"`
       } `json:"selector"`
     }
 
-    jsonStr := AddrSelector{Selector: struct{Addr string `json:"address.addr"`}{Addr:address.Addr,},}
+    jsonStr := AddrSelector{Selector: struct{Addr string `json:"ipaddress.addr"`}{Addr:address.Addr,},}
     fmt.Println("jsonStr for address selector ", jsonStr)
     jsonValue, _ := json.Marshal(jsonStr)
     resp, err := http.Post(searchURL, "application/json", bytes.NewBuffer(jsonValue))
