4c0bcc9ad6e755ccd02efd5fc0c352ab3f286df4
Maxwell-McAdam <Maxwell.McAdam@ibm.com>
2019-04-24 20:17:31 -0400

    print exec cmd

--- src/wiotp.go
+++ src/wiotp.go
@@ -13,8 +13,12 @@ func newAlert(wg *sync.WaitGroup, alertType string, alertDevice string) {
 	//pubClientOptions.Username = wiotp_org
 	//pubClientOptions.Password = wiotp_auth_token
 	//pubClient := mqtt.NewClient(pubClientOptions)
+
+	//
+
 	cmdFunction := "mosquitto_pub"
 	alertString := "{" + strconv.Quote("alerttype") + ":" + strconv.Quote(alertType) + "," + strconv.Quote("alertdevice") + ":" + strconv.Quote(alertDevice) + "}"
+	fmt.Println(cmdFunction, "-h", os.ExpandEnv("WIOTP_ORG")+".messaging.internetoftings.ibmcloud.com", "-p", "8883", "-i", "d:"+os.ExpandEnv("WIOTP_ORG")+":"+os.ExpandEnv("WIOTP_DEVICE_TYPE")+":"+os.ExpandEnv("WIOTP_DEVICE_ID"), "-u", "use-token-auth", "-P", os.ExpandEnv("WIOTP_DEVICE_TOKEN"), "--capath", "/etc/ssl/certs", "-t", "iot-2/evt/status/fmt/json", "-m", alertString, "-d")
 	cmd := exec.Command(cmdFunction, "-h", os.ExpandEnv("WIOTP_ORG")+".messaging.internetoftings.ibmcloud.com", "-p", "8883", "-i", "d:"+os.ExpandEnv("WIOTP_ORG")+":"+os.ExpandEnv("WIOTP_DEVICE_TYPE")+":"+os.ExpandEnv("WIOTP_DEVICE_ID"), "-u", "use-token-auth", "-P", os.ExpandEnv("WIOTP_DEVICE_TOKEN"), "--capath", "/etc/ssl/certs", "-t", "iot-2/evt/status/fmt/json", "-m", alertString, "-d")
 
 	mosqPubOutput, err := cmd.CombinedOutput()
